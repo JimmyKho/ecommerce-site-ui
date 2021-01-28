96e4d417d5a93576a88f50e88ed46e3c8bb70e06
JimmyKho <kho.jimmy@gmail.com>
2021-01-28 16:51:19 +0800

    Color Option Tooltip

--- components/product/ColorOptions.js
+++ components/product/ColorOptions.js
@@ -1,3 +1,5 @@
+import { OverlayTrigger, Tooltip } from "react-bootstrap"
+
 import styles from "./ColorOptions.module.scss"
 
 const ColorOptions = (props) => {
@@ -6,7 +8,14 @@ const ColorOptions = (props) => {
   return (
     <div className={styles.ColorOptions}>
       {options.map((option, index) => (
-        <div key={"Item" + index} className={styles.ColorOptions_Item} style={{ backgroundColor: option.code }} />
+        <OverlayTrigger
+          key={"Item" + index}
+          placement="top"
+          delay={{ show: 250, hide: 400 }}
+          overlay={<Tooltip>{option.name}</Tooltip>}
+        >
+          <div className={styles.ColorOptions_Item} style={{ backgroundColor: option.code }} />
+        </OverlayTrigger>
       ))}
     </div>
   )
