2809dbbc0e894b6da0ae662cbc8f5d6be91d7895
JimmyKho <kho.jimmy@gmail.com>
2021-01-29 21:17:12 +0800

    Update Product Card Styling

--- components/product/ProductCard.js
+++ components/product/ProductCard.js
@@ -1,5 +1,8 @@
 import { useState } from "react"
 import { Modal, Button } from "react-bootstrap"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faPlus } from "@fortawesome/free-solid-svg-icons"
+import { faStar } from "@fortawesome/free-regular-svg-icons"
 
 import CardPrice from "./CardPrice"
 import ColorOptions from "./ColorOptions"
@@ -10,33 +13,42 @@ import styles from "./ProductCard.module.scss"
 
 const ProductCard = (props) => {
   const [show, setShow] = useState(false)
+  const [size, setSize] = useState()
+  const [color, setColor] = useState()
 
-  const { name = "", price = 0, oriPrice = 0, attr = {} } = props.data ?? {}
+  const { name = "", price = 0, oriPrice = 0, attr = {}, info = {} } = props.data ?? {}
   const { colors = [], sizes = [] } = attr
+  const { rating = 0 } = info
 
   const showFn = () => setShow(true)
   const closeFn = () => setShow(false)
 
   return (
     <>
-      <div className={styles.ProductCard} onClick={showFn}>
-        <div className={styles.ProductCard_ImageCont}>
+      <div className={styles.ProductCard}>
+        <div className={styles.ProductCard_ImageCont} onClick={showFn}>
           <img className={styles.ProductCard_Image} src="/prod1_grey3.jpg" />
         </div>
         <div className={styles.ProductCard_Content}>
-          <div className={styles.ProductCard_Heading}>
+          <div className={styles.ProductCard_Heading} onClick={showFn}>
             <div className={styles.ProductCard_Heading_Title}>{name}</div>
             <div className={styles.ProductCard_Heading_Price}>
               <CardPrice price={price} oriPrice={oriPrice} />
             </div>
           </div>
           <div className={styles.ProductCard_Option}>
-            <ColorOptions options={colors} />
-            <SizeOptions options={sizes} />
+            <ColorOptions selected={color} onSelect={(item) => setColor(item)} options={colors} />
+            <SizeOptions selected={size} onSelect={(item) => setSize(item)} options={sizes} />
           </div>
-          <div className={styles.ProductCard_Footer}>
-            <div>Rating</div>
-            <div>Buy</div>
+          <div className={styles.ProductCard_Footer} onClick={showFn}>
+            <div className={styles.ProductCard_Footer_Rating}>
+              <FontAwesomeIcon className={styles.ProductCard_Footer_Rating_Icon} icon={faStar} />
+              {rating}
+            </div>
+            <div className={styles.ProductCard_Footer_Buy}>
+              BUY
+              <FontAwesomeIcon className={styles.ProductCard_Footer_Buy_Icon} icon={faPlus} />
+            </div>
           </div>
         </div>
       </div>
